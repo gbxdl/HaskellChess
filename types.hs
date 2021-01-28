e27511b3f7308dc564c8d540d35dd1e920ab8522
gbxdl <daas.leer@gmail.com>
2021-01-28 12:42:58 +0100

    introduced Piece type

--- types.hs
+++ types.hs
@@ -28,34 +28,54 @@ type HalfMove = Int
 type FullMove = Int
 
 data Color = White | Black deriving (Eq)
-
--- instance Read Color where
---    readsPrec _ c = if c=="w" then [(White, "")] else [(Black, "")]
-
--- instance Read Bool where
---   readPrec =
---     parens
---     ( do L.Ident s <- lexP
---          case s of
---            "True"  -> return True
---            "False" -> return False
---            _       -> pfail
--- )
-
+data Piece = King Color | Queen Color| Rook Color | Bishop Color | Knight Color | Pawn Color | Empty deriving (Eq) 
+-- data Enpassant = 
 instance Read Color where
    readsPrec _ "w" = [(White,"")]
    readsPrec _ "b" = [(Black,"")]
    readsPrec _ _ = []
 
--- readColor :: String -> Color
--- readColor "w" = White 
--- readColor "b" = Black
-   
 instance Show Color where
    show White = "w"
    show Black = "b"
--- data Piece = K | Q | R | B | K | P | k | q | 
 
+instance Show Piece where
+   show (King color) =  case color of
+                           White -> "K"
+                           Black -> "k"
+   show (Queen color) = case color of
+                           White -> "Q"
+                           Black -> "q"
+   show (Rook color) = case color of
+                           White -> "R"
+                           Black -> "r"
+   show (Bishop color) = case color of
+                           White -> "B"
+                           Black -> "b"
+   show (Knight color) = case color of
+                           White -> "N"
+                           Black -> "n"
+   show (Pawn color) = case color of
+                           White -> "P"
+                           Black -> "p"
+   show Empty = "."
+
+instance Read Piece where
+   readsPrec _ "K" = [(King White,"")]
+   readsPrec _ "k" = [(King White,"")]
+   readsPrec _ "Q" = [(Queen White,"")]
+   readsPrec _ "q" = [(Queen Black,"")]
+   readsPrec _ "R" = [(Rook White,"")]
+   readsPrec _ "r" = [(Rook Black,"")]
+   readsPrec _ "B" = [(Bishop White,"")]
+   readsPrec _ "b" = [(Bishop Black,"")]
+   readsPrec _ "N" = [(Knight White,"")]
+   readsPrec _ "n" = [(Knight Black,"")]
+   readsPrec _ "P" = [(Pawn White,"")]
+   readsPrec _ "p" = [(Pawn Black,"")]
+   readsPrec _ "." = [(Empty,"")]
+   readsPrec _ _ = []
+   
 isBoardPosition :: Square -> Bool
 isBoardPosition pos = pos `elem` [(x,y)| x<-[0..7], y<-[0..7]]
 
